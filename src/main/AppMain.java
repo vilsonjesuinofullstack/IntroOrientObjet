841dbace30b2a90aa67b2f40c7a81b4f1ff9ed16
Vilson Jesuino de Lima Junior <vilsonjesuino@outllook.com.br>
2022-07-16 11:58:00 -0300

    Entendendo os métodos comuns Getters e Setters

--- src/main/AppMain.java
+++ src/main/AppMain.java
@@ -8,11 +8,11 @@ public class AppMain {
 
         /*new aluno é uma instancia, ou criação de um objeto*/
         /*aluno1 é uma referencia para o objeto aluno*/
-        Aluno aluno1 = new Aluno("Pedro", 28);
-        aluno1.nome = "Vilson Jesuino";
-        aluno1.idade = 29;
+        Aluno aluno1 = new Aluno();
+        aluno1.setNome("Junior");
+        aluno1.setIdade(30);
 
-        System.out.println("Nome do Aluno: " + aluno1.nome);
-        System.out.println("Idade do Aluno: " + aluno1.idade);
+        System.out.println("Nome do Aluno: " + aluno1.getNome());
+        System.out.println("Idade do Aluno: " + aluno1.getIdade());
     }
 }
