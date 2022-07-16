e01d991aa197f741337ea880516054967f3bfaf7
Vilson Jesuino de Lima Junior <vilsonjesuino@outllook.com.br>
2022-07-16 18:50:07 -0300

    Criando entrada de dados com JOptionPane

--- src/main/AppMain.java
+++ src/main/AppMain.java
@@ -2,19 +2,30 @@ package main;
 
 import classes.Aluno;
 
+import javax.swing.*;
+
 public class AppMain {
 
     public static void main(String[] args) {
 
+        /*Entrada de Dados com JOptionPane*/
+
+        String nome = JOptionPane.showInputDialog("Qual o nome do aluno: ");
+        String idade = JOptionPane.showInputDialog("Qual a idade do aluno: ");
+        String nota1 = JOptionPane.showInputDialog("Qual a nota 1 do aluno: ");
+        String nota2 = JOptionPane.showInputDialog("Qual a nota 2 do aluno: ");
+        String nota3 = JOptionPane.showInputDialog("Qual a nota 3 do aluno: ");
+        String nota4 = JOptionPane.showInputDialog("Qual a nota 4 do aluno: ");
+
         /*new aluno é uma instancia, ou criação de um objeto*/
         /*aluno1 é uma referencia para o objeto aluno*/
         Aluno aluno1 = new Aluno();
-        aluno1.setNome("Junior");
-        aluno1.setIdade(30);
-        aluno1.setNota1(90);
-        aluno1.setNota2(95);
-        aluno1.setNota3(95);
-        aluno1.setNota4(86);
+        aluno1.setNome(nome);
+        aluno1.setIdade(Integer.parseInt(idade));
+        aluno1.setNota1(Double.parseDouble(nota1));
+        aluno1.setNota2(Double.parseDouble(nota2));
+        aluno1.setNota3(Double.parseDouble(nota3));
+        aluno1.setNota4(Double.parseDouble(nota4));
 
         System.out.println("Nome do Aluno: " + aluno1.getNome());
         System.out.println("Idade do Aluno: " + aluno1.getIdade());
