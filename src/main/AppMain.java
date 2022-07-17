8d8a3e2193a8b4da2266b551bf38f040812cec08
Vilson Jesuino de Lima Junior <vilsonjesuino@outllook.com.br>
2022-07-17 15:41:34 -0300

    Criando lista com HashMap em lista de aluno

--- src/main/AppMain.java
+++ src/main/AppMain.java
@@ -2,9 +2,11 @@ package main;
 
 import classes.Aluno;
 import classes.Disciplina;
+import constantes.StatusAluno;
 
 import javax.swing.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class AppMain {
@@ -12,6 +14,8 @@ public class AppMain {
     public static void main(String[] args) {
 
         List<Aluno> alunos = new ArrayList<Aluno>();
+
+        HashMap<String, List<Aluno>> maps = new HashMap<String, List<Aluno>>();
 
         for (int qtd = 1; qtd <= 1; qtd++) {
 
@@ -54,11 +58,45 @@ public class AppMain {
             alunos.add(aluno);
         }
 
+        maps.put(StatusAluno.APROVADO_NOTA_MAXIMA, new ArrayList<Aluno>());
+        maps.put(StatusAluno.APROVADO, new ArrayList<Aluno>());
+        maps.put(StatusAluno.RECUPERACAO, new ArrayList<Aluno>());
+        maps.put(StatusAluno.REPROVADO, new ArrayList<Aluno>());
+
         for (Aluno aluno : alunos) {
             System.out.println(aluno);
             System.out.println("Calculo Média Nota: " + aluno.getMediaNota());
             System.out.println("Resultado da Aprovação: " + aluno.getAprovado());
+
             System.out.println("------------------------------------------------------------------");
+
+            if (aluno.getAprovado().equalsIgnoreCase(StatusAluno.APROVADO_NOTA_MAXIMA)) {
+                maps.get(StatusAluno.APROVADO_NOTA_MAXIMA).add(aluno);
+            } else if (aluno.getAprovado().equalsIgnoreCase(StatusAluno.APROVADO)) {
+                maps.get(StatusAluno.APROVADO).add(aluno);
+            } else if (aluno.getAprovado().equalsIgnoreCase(StatusAluno.RECUPERACAO)) {
+                maps.get(StatusAluno.RECUPERACAO).add(aluno);
+            } else if (aluno.getAprovado().equalsIgnoreCase(StatusAluno.REPROVADO)) {
+                maps.get(StatusAluno.REPROVADO).add(aluno);
+            }
+        }
+
+        System.out.println("---------------------APROVADO COM NOTA MAXIMA---------------------");
+        for (Aluno aluno : maps.get(StatusAluno.APROVADO_NOTA_MAXIMA)) {
+            System.out.println("Resultado = " + aluno.getAprovado() + " com média de = " + aluno.getMediaNota());
+        }
+        System.out.println("---------------------APROVADO-------------------------------------");
+        for (Aluno aluno : maps.get(StatusAluno.APROVADO)) {
+            System.out.println("Resultado = " + aluno.getAprovado() + " com média de = " + aluno.getMediaNota());
+        }
+        System.out.println("---------------------RECUPERACAO----------------------------------");
+        for (Aluno aluno : maps.get(StatusAluno.RECUPERACAO)) {
+            System.out.println("Resultado = " + aluno.getAprovado() + " com média de = " + aluno.getMediaNota());
+        }
+
+        System.out.println("---------------------REPROVADO------------------------------------");
+        for (Aluno aluno : maps.get(StatusAluno.REPROVADO)) {
+            System.out.println("Resultado = " + aluno.getAprovado() + " com média de = " + aluno.getMediaNota());
         }
     }
 }
